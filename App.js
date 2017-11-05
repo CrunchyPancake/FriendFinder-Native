3583521f3588c1b401134dba570a7a76eb2a2612
Jonathan <jonathan.henriksen@yandex.com>
2017-11-05 16:50:54 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { StyleSheet, Text, View, MapView, Modal, Alert, TextInput } from 'react-native';
+import PrettyButton from './components/PrettyButton';
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { name: '', distance: '' };
+
+  }
+  render() {
+    return (
+      <View style={styles.main_container}>
+
+        <View style={styles.container_row}>
+          <Text style={styles.text}>
+            Nickname
+          </Text>
+          <TextInput style={styles.inputField} placeholder="Enter your name"
+            onChangeText={(name) => this.setState({ name })} >
+          </TextInput>
+        </View>
+
+        <View style={styles.container_row}>
+          <Text style={styles.text}>
+            Distance
+          </Text>
+          <TextInput style={styles.inputField} placeholder="Kilometers"
+            onChangeText={(distance) => this.setState({ distance })} >
+          </TextInput>
+
+        </View>
+
+
+        <View style={styles.container_row}>
+          <PrettyButton title='Submit'
+            onPress={() => Alert.alert(this.state.distance)} />
+        </View>
+      </View>
+
+
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'linen',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  container_row: {
+    flexDirection: 'row',
+    padding: 5,
+    alignItems: 'center',
+  },
+  inputField: {
+    height: 70,
+    width: 170,
+    fontSize: 20
+  },
+  text: {
+    flex: 1,
+    fontSize: 20,
+    padding: 20,
+    opacity: 0.6
+  }
+
+});
---
+++ components/PrettyButton.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Text, TouchableHighlight, StyleSheet, Image } from 'react-native';
+
+export default class PrettyButton extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <TouchableHighlight onPress={this.props.onPress} activeOpacity={0.6} underlayColor={'white'}>
+                <Image
+                    style={styles.touchable}
+                    source={require('./button.png')}>
+                    <Text style={styles.text}>
+                        {this.props.title}
+                    </Text>
+                </Image>
+            </TouchableHighlight>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    touchable: {
+        height: 70,
+        width: 180,
+        opacity: 0.8,
+        alignItems: 'center'
+
+    },
+    text: {
+        flex: 1,
+        fontSize: 20,
+        padding: 20,
+        opacity: 0.6
+    }
+})
+
